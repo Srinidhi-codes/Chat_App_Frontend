f99899cc3f090e20bedf4c8b5c35ae672bc37a8c
srii-codes <srinidhiachar2002@gmail.com>
2025-05-23 13:14:22 +0530

    Changed default routes

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
